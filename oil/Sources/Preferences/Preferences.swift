09217ee192f22a36c31a336e0c8341db0e5a83ba
Maximus Cisneros <mxcrcpu@gmail.com>
2023-05-15 14:51:29 -0700

    Update: Establishing Preferences Methodology

--- oil/Sources/Preferences/Preferences.swift
+++ oil/Sources/Preferences/Preferences.swift
@@ -8,43 +8,7 @@
 import Foundation
 import Defaults
 
-/*
-internal struct Preferences {
-    internal enum Keys: String {
-        case shouldDisplayCpuNumber
-        case shouldDisplayCpuBarGraph
-    }
-    
-    static subscript<T>(_ key: Keys) -> T {
-        get {
-            guard let value = UserDefaults.standard.value(
-                forKey: key.rawValue
-            ) as? T else {
-                switch key {
-                    case .shouldDisplayCpuNumber:
-                        return true as! T
-                    case .shouldDisplayCpuBarGraph:
-                        return true as! T
-                }
-            }
-            return value
-        }
-        set {
-            UserDefaults.standard.setValue(
-                newValue,
-                forKey: key.rawValue
-            )
-        }
-    }
-    
-    static func reset() {
-        Preferences[.shouldDisplayCpuNumber] = true
-        Preferences[.shouldDisplayCpuBarGraph] = true
-    }
-}
-*/
-
 extension Defaults.Keys {
-    static let shouldDisplayCpuNumber = Defaults.Key<Bool>("shouldDisplayCpuNumber", default: false)
-    static let shouldDisplayCpuNumber = Defaults.Key<Bool>("shouldDisplayCpuNumber", default: false
+    static let shouldDisplayCpuNumber = Key<Bool>("shouldDisplayCpuNumber", default: true)
+    static let shouldDisplayCpuGraph = Key<Bool>("shouldDisplayCpuGraph", default: true)
 }
--- oil/Sources/Preferences/oilWidgetPreferencePane.swift
+++ oil/Sources/Preferences/oilWidgetPreferencePane.swift
@@ -7,47 +7,43 @@
 
 //import Foundation
 import Cocoa
+import Defaults
 import PockKit
 
 class oilWidgetPreferencePane:
     NSViewController,
-    NSTextFieldDelegate,
     PKWidgetPreference {
-        static var nibName: NSNib.Name = "oilWidgetPreferencePane"
+    static var nibName: NSNib.Name = "oilWidgetPreferencePane"
+    @IBOutlet weak var showCpuNumber: NSButton!
+    @IBOutlet weak var showCpuGraph: NSButton!
+    
+    func reset() {
+        loadCheckboxState()
+    }
         
-        @IBOutlet weak var showCpuNumber: NSButton!
-        @IBOutlet weak var showCpuGraph: NSButton!
-        
-        func reset() {
-            Preferences.reset()
-            loadCheckboxState()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.superview?.wantsLayer = true
+        self.view.wantsLayer = true
+        self.loadCheckboxState()
+    }
+    
+    private func loadCheckboxState() {
+        self.showCpuNumber.state = Defaults[.shouldDisplayCpuNumber] ? .on : .off
+        self.showCpuGraph.state = Defaults[.shouldDisplayCpuGraph] ? .on : .off
+    }
+    
+    @IBAction func didChangeCheckboxValue(_ checkbox: NSButton) {
+        var key: Defaults.Key<Bool>
+        switch checkbox.tag {
+            case 1:
+                key = .shouldDisplayCpuNumber
+            case 2:
+                key = .shouldDisplayCpuGraph
+            default:
+                return
         }
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            self.view.superview?.wantsLayer = true
-            self.view.wantsLayer = true
-            self.loadCheckboxState()
-        }
-        
-        private func loadCheckboxState() {
-            self.showCpuNumber.state =
-                Preferences[.shouldDisplayCpuNumber] ? .on : .off
-            self.showCpuGraph.state =
-                Preferences[.shouldDisplayCpuBarGraph] ? .on : .off
-        }
-        
-        @IBAction func didChangeCheckboxValue(_ checkbox: NSButton) {
-            var key: Preferences.Keys
-            switch checkbox.tag {
-                case 0:
-                    key = .shouldDisplayCpuNumber
-                case 1:
-                    key = .shouldDisplayCpuBarGraph
-                default:
-                    return
-            }
-            Preferences[key] = checkbox.state == .on
-            loadCheckboxState()
-        }
+        Defaults[key] = checkbox.state == .on
+        loadCheckboxState()
+    }
 }
