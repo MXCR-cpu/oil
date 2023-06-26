32708e01cf15cda78ad1f0c37344bc6b34ff2d74
Maximus Cisneros <mxcrcpu@gmail.com>
2023-06-25 18:47:05 -0700

    > Fix  Altering Package.swift file

--- Package.swift
+++ Package.swift
@@ -1,1 +1,30 @@
-// swift-tools-version:5.8.0
+// swift-tools-version:5.8
+
+let package = Package(
+	name: "Defaults",
+	platforms: [
+		.macOS(.v10_15),
+		.iOS(.v13),
+		.tvOS(.v13),
+		.watchOS(.v6)
+	],
+	products: [
+		.library(
+			name: "Defaults",
+			targets: [
+				"Defaults"
+			]
+		)
+	],
+	targets: [
+		.target(
+			name: "Defaults"
+		),
+		.testTarget(
+			name: "DefaultsTests",
+			dependencies: [
+				"Defaults"
+			]
+		)
+	]
+)
